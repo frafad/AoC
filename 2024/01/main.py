8d6952c818422019752e0e8d6c1df3249eba5b46
frafad <fra.fadini@gmail.com>
2024-12-05 23:53:16 +0100

    organize folder structure

--- 2024/01/main.py
+++ 2024/01/main.py
@@ -3,7 +3,7 @@ from collections import Counter
 
 
 def load_data():
-    data = np.loadtxt("input.txt", dtype=int)
+    data = np.loadtxt("/home/frafad/code/AoC/2024/01/input.txt", dtype=int)
     return data[:, 0], data[:, 1]
 
 
@@ -12,21 +12,18 @@ def answer_one(arr1, arr2):
     arr2 = np.sort(arr2)
     diff = np.abs(arr1 - arr2)
     result = np.sum(diff)
-    print("01: ", result)
-
-def answer_two(arr1, arr2):
+    print("1.1: ", result)
     c = Counter(arr2)
     sim_scores = np.zeros(shape=(len(arr1),), dtype=int)
     for idx, x in enumerate(arr1):
         sim_scores[idx] = x * c.get(x, 0)
     result = np.sum(sim_scores)
-    print("02: ", result)
+    print("1.2: ", result)
 
 
 def main():
     arr1, arr2 = load_data()
     answer_one(arr1, arr2)
-    answer_two(arr1, arr2)
 
 if __name__ == "__main__":
     main()
